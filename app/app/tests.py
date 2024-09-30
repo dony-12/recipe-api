a7269140ce7894fca78f32b4b8b0d620bde6a0ba
dony_12 <donyenuforo@gmail.com>
2024-09-30 13:34:37 +0100

    Updated Docker compose and CI/CD 2

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,8 +2,3 @@
 
 from django.test import SimpleTestCase
 from app import add
-
-class AddTest(SimpleTestCase):
-    def test_add(self):
-        res = add.add(7,6)
-        self.assertEqual(res, 11)
